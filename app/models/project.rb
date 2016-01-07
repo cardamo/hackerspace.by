af393734623b453fcb8a7ed78af1ca5c190d4cad
Yahor Zhuchkou <jagor.z@gmail.com>
2016-01-07 15:11:46 +0300

    rspec changes, annotations for models

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,20 +3,20 @@
 # Table name: projects
 #
 #  id                 :integer          not null, primary key
-#  name               :string(255)
+#  name               :string
 #  short_desc         :text
 #  full_desc          :text
-#  image              :string(255)
+#  image              :string
 #  created_at         :datetime
 #  updated_at         :datetime
-#  photo_file_name    :string(255)
-#  photo_content_type :string(255)
+#  photo_file_name    :string
+#  photo_content_type :string
 #  photo_file_size    :integer
 #  photo_updated_at   :datetime
 #  user_id            :integer
-#  markup_type        :string(255)      default("html")
+#  markup_type        :string           default("html")
 #  public             :boolean          default(FALSE)
-#  project_status     :string(255)      default("активный")
+#  project_status     :string           default("активный")
 #
 
 class Project < ActiveRecord::Base
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,33 @@
+# == Schema Information
+#
+# Table name: projects
+#
+#  id                 :integer          not null, primary key
+#  name               :string
+#  short_desc         :text
+#  full_desc          :text
+#  image              :string
+#  created_at         :datetime
+#  updated_at         :datetime
+#  photo_file_name    :string
+#  photo_content_type :string
+#  photo_file_size    :integer
+#  photo_updated_at   :datetime
+#  user_id            :integer
+#  markup_type        :string           default("html")
+#  public             :boolean          default(FALSE)
+#  project_status     :string           default("активный")
+#
+
 require 'rails_helper'
 
 describe Project do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relations and validations" do
+
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:short_desc) }
+    it { should validate_presence_of(:project_status) }
+    
+
+  end
 end
